626b67175c9eb7f4bca2dd5451ada0f6b2ab1c7b
Kengii <kengii9431@gmail.com>
2023-12-10 12:40:28 +0300

    ProjectCreated

---
+++ WeatherAppTestTask/WeatherAppTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppTestTask
+//
+//  Created by Владимир Данилович on 10.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
